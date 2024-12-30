cbfdb48963a980db51fb294c8322a9934de995ad
bocil-termux <pentesting833@gmail.com>
2024-12-30 15:04:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tidak perlu banyak JS karena animasi dikelola oleh CSS
+// Tetapi jika ingin menambahkan interaktivitas atau fungsionalitas lebih lanjut, ini adalah tempatnya
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("EYEFOX page loaded.");
+});
